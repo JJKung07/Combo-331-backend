9a95124a5fcde1c7025571604fddef9005a95810
JJKung <jj10102546@gmail.com>
2024-08-29 23:49:57 +0700

    Finish 4.6

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,5 +1,6 @@
 package se331.lab.rest.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -96,13 +97,15 @@ public class EventController {
         page = page == null?1:page;
         Integer firstIndex = (page-1)*perPage;
         List<Event> output = new ArrayList<>();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("x-total-count",String.valueOf(eventsList.size()));
         try {
             for (int i = firstIndex; i < firstIndex+perPage; i++) {
                 output.add(eventsList.get(i));
             }
-            return ResponseEntity.ok(output);
+            return new ResponseEntity<>(output, responseHeaders, HttpStatus.OK);
         } catch (IndexOutOfBoundsException ex) {
-            return ResponseEntity.ok(ex.getMessage());
+            return new ResponseEntity<>(null, responseHeaders, HttpStatus.OK);
         }
     }
 
