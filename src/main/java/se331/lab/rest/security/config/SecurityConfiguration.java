453d17b63848f4449a572e251a6b2582de492007
JJKung07 <jiraphat_l@cmu.ac.th>
2024-10-11 01:00:41 +0700

    Lab 12 2.6

--- src/main/java/se331/lab/rest/security/config/SecurityConfiguration.java
+++ src/main/java/se331/lab/rest/security/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package se331.lab.security.config;
+package se331.lab.rest.security.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -17,38 +17,38 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-  private final JwtAuthenticationFilter jwtAuthFilter;
-  private final AuthenticationProvider authenticationProvider;
-  private final LogoutHandler logoutHandler;
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    http.headers((headers) -> {
-      headers.frameOptions((frameOptions) -> frameOptions.disable());
-    });
-    http
-            .csrf((crsf) -> crsf.disable())
-            .authorizeHttpRequests((authorize) -> {
+        http.headers((headers) -> {
+            headers.frameOptions((frameOptions) -> frameOptions.disable());
+        });
 
-              authorize.anyRequest().authenticated();
-            })
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> {
+                    authorize.requestMatchers("/api/v1/auth/**").permitAll()
+                            .anyRequest().authenticated();
+                })
 
-            .sessionManagement((session) ->{
-              session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-            })
+                .sessionManagement((session) ->{
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
 
 
-            .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-            .logout((logout) -> {
-              logout.logoutUrl("/api/v1/auth/logout");
-              logout.addLogoutHandler(logoutHandler);
-              logout.logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
-            })
-    ;
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout((logout) -> {
+                    logout.logoutUrl("/api/v1/auth/logout");
+                    logout.addLogoutHandler(logoutHandler);
+                    logout.logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
+                })
+        ;
 
-    return http.build();
+        return http.build();
 
-  }
+    }
 }
